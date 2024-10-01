3d1750ef98d576cf4de24c0c082f4a8bc84389b9
Bruce <irabruce10@gmail.com>
2024-10-01 16:24:37 +0200

    Adding router

--- app/products/page.js
+++ app/products/page.js
@@ -1,3 +1,32 @@
+import Link from 'next/link';
+import { getProducts } from '../database/product';
+
 export default function productPage() {
-  return <div> Product page</div>;
+  const products = getProducts();
+  return (
+    <div>
+      <h1>Our top Products</h1>
+
+      {products.map((product) => {
+        return (
+          <div key={`product-${product.id}`}>
+            <Link href={`/products/${product.id}`}>
+              <h2>{product.title}</h2>
+              <img
+                src={product.image}
+                alt={product.title}
+                width={300}
+                height={300}
+              />
+            </Link>
+            <p>{product.description}</p>
+            <p>Price: {product.price}</p>
+            <p>
+              Rating: {product.rating.rate} ({product.rating.count} votes)
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
