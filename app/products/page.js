b92a54eed9ba10c9c6f5409251ad78d3dccb2768
Bruce <irabruce10@gmail.com>
2024-10-14 18:57:00 +0200

    Adding Testing E2E

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import styles from './product.module.scss';
 import { getProductsInsecure } from '../database/product.ts';
+import Image from 'next/image';
 export const metadata = {
   title: 'Product',
 };
@@ -17,23 +18,26 @@ export default async function productPage() {
           return (
             <div
               key={`product-${product.id}`}
-              data-test-id="product-<product id>"
+              data-test-id={`product-${product.id}`}
             >
               <Link
                 href={`/products/${product.id}`}
                 data-test-id="products-link"
                 className={styles.product_link}
               >
-                <img
+                <Image
                   src={product.image}
                   alt={product.title}
                   width={250}
                   height={300}
+                  data-test-id="product-image"
                 />{' '}
                 <h2>{product.name}</h2>
               </Link>
 
-              <p className={styles.price}>Price: {product.price}</p>
+              <p data-test-id="product-price" className={styles.price}>
+                Price: {product.price}
+              </p>
             </div>
           );
         })}
--- playwright/AddToCart.spec.ts
+++ playwright/AddToCart.spec.ts
@@ -9,4 +9,14 @@ test('add to cart test', async ({ page }) => {
   await expect(
     page.getByRole('heading', { name: 'Our top Products' }),
   ).toBeVisible();
+  await expect(page.getByTestId('product-image')).toBeVisible();
+  // await page.getByTestId('product-2').click();
+
+  // await page
+  //   .getByRole('link', {
+  //     name: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+  //   })
+  //   .click();
+
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
 });
