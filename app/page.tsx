cfdfcc76aea44b9f27786dd96b40949bc17fac65
Bruce <irabruce10@gmail.com>
2024-10-18 14:41:40 +0200

    Creating cd/ci

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,7 @@ export default function Home() {
   return (
     <div className="homePage">
       <h1 className="title">Welcome to our E-soko yacu</h1>
-      {/* <Image
-        className="img"
-        src={bg1}
-        placeholder="blur"
-        quality={80}
-        alt="E-soko"
-      /> */}
+      <Image className="img" src={bg1} quality={80} alt="E-soko" />
     </div>
   );
 }
