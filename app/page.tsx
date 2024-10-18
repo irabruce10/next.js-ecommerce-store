0ae45a1213c71e400fa372ad0a7688df5beae4f0
Bruce <irabruce10@gmail.com>
2024-10-18 14:24:41 +0200

    Adding config file

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import bg1 from '../public/images/bg1.jpg';
 export default function Home() {
   return (
     <div className="homePage">
-      <h1 className="title">Welcome to our E-soko ue</h1>
+      <h1 className="title">Welcome to our E-soko yacu</h1>
       {/* <Image
         className="img"
         src={bg1}
