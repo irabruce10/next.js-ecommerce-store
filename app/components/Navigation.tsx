35580f41dc5eb4d9e17c3d5d6332aca052afa2b6
Bruce <irabruce10@gmail.com>
2024-10-20 17:12:04 +0200

    Fixing some error

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -14,7 +14,7 @@ export default function Navigation() {
         <li>
           <Link href="/products">Products</Link>
         </li>
-        <li className="cart">
+        <li className="cart" data-test-id="products-link">
           <Link href="/cart" data-test-id="cart-link">
             <CartCount />
           </Link>
