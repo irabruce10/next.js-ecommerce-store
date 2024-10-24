d3e71983a551e3d3a669356ae9a1c5fc8766d99a
Bruce <irabruce10@gmail.com>
2024-10-24 07:34:28 +0200

    Fixing some error

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <div className="homePage">
-      <h1 className="title">Welcome to our E-soko yacu</h1>
+      <h1 className="title">Welcome to our E-soko yacu ...</h1>
       {/* <Image className="img" src={bg1} quality={80} alt="E-soko" /> */}
     </div>
   );
