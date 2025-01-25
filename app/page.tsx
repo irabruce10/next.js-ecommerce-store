cb79b67a30bbf5139c6aba9ec831dcac4a794641
Bruce <irabruce10@gmail.com>
2025-01-25 14:42:32 +0100

    Fix some bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-// import Image from '../node_modules/next/image';
-// import bg1 from '../public/images/bg1.jpg';
+import Image from '../node_modules/next/image';
+import bg1 from '../public/images/bg1.jpg';
 
 export default function Home() {
   return (
     <div className="homePage">
-      <h1 className="title">Welcome to our E-soko yacu ...</h1>
-      {/* <Image className="img" src={bg1} quality={80} alt="E-soko" /> */}
+      <h1 className="title">Welcome to our E-soko yacu difsdofds] [fvsd</h1>
+      <Image className="img" src={bg1} quality={80} alt="E-soko" />
+      <p className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec velit
+        nunc. Sed non enim neque. Aliquam et urna vel velit tincidunt gravida.
+        Nulla facilisi.
+      </p>
     </div>
   );
 }
