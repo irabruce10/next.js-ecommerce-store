14d2a666ea4c219f385cf8e9c1995a178d911eb8
Bruce <irabruce10@gmail.com>
2025-01-26 14:11:24 +0100

    Fix some bug

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import bg1 from '../public/images/bg1.jpg';
 export default function Home() {
   return (
     <div className="homePage">
-      <h1 className="title">Welcome to our E-soko yacu difsdofds] [fvsd</h1>
+      <h1 className="title">Welcome to our E-soko yacu.</h1>
       <Image className="img" src={bg1} quality={80} alt="E-soko" />
       <p className="description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec velit
-        nunc. Sed non enim neque. Aliquam et urna vel velit tincidunt gravida.
-        Nulla facilisi.
+        E-soko is a marketplace where you can buy, sell, and trade products. We
+        have a wide variety of products, from electronics to clothing, and we're
+        always looking for new and exciting items to sell!
       </p>
     </div>
   );
