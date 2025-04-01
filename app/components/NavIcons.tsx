af02ec9dc793c82493038848e7b8808d9a6bbf3e
Bruce <irabruce10@gmail.com>
2025-04-01 20:37:27 +0200

    Add list file

---
+++ app/components/NavIcons.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function NavIcons() {
+  return <div>NavIcons</div>;
+}
--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -28,18 +28,38 @@
 
 import Link from 'next/link';
 
+import Image from 'next/image';
+
 import Menu from './Menu';
+import SearchBar from './SearchBar';
+import NavIcons from './NavIcons';
 
 export default function Navigation() {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
-      <div className="flex  items-center justify-between h-full ">
+    <div className=" h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      <div className="h-full flex  items-center justify-between md:hidden  ">
         {/* Mobile */}
         <Link href="/">
           <div className=" text-2xl tracking-wide">BuyNow</div>
         </Link>
 
         <Menu />
+      </div>
+      {/* Bigger Screen */}
+      <div className="hidden md:flex items-center justify-between h-full gap-8">
+        {/* left */}
+        <div className=" w-1/3">
+          <Link href="/" className="flex items-center gap-3 ">
+            <Image src="/logo.png" alt="Logo Image" width={24} height={24} />
+            <div className=" text-2xl tracking-wide">BuyNow</div>
+          </Link>
+        </div>
+
+        {/* right */}
+        <div className=" w-2/3 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
+        </div>
       </div>
     </div>
   );
