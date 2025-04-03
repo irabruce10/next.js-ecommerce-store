730f629c80dad88ea9394750a8704fa32fc9b1b6
Bruce <irabruce10@gmail.com>
2025-04-03 15:20:11 +0200

    Create Product Images

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,16 @@
+import ProductImages from '../components/ProductImages';
+
 function page() {
-  return <div>page SLug</div>;
+  return (
+    <div className=" px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* Image  */}
+      <div className=" w-full  lg:w-1/2 lg:sticky top-20 h-max ">
+        <ProductImages />
+      </div>
+      {/* text */}
+      <div className=" w-full lg:w-1/2 flex flex-col gap-6"></div>
+    </div>
+  );
 }
 
 export default page;
---
+++ app/components/ProductImages.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+const images = [
+  {
+    id: '1',
+    url: 'https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg',
+    alt: 'Product 1',
+  },
+  {
+    id: '2',
+    url: 'https://images.pexels.com/photos/31303658/pexels-photo-31303658/free-photo-of-serene-woman-sitting-by-green-water.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    alt: 'Product 2',
+  },
+  {
+    id: '3',
+    url: 'https://images.pexels.com/photos/31197584/pexels-photo-31197584/free-photo-of-elegant-portrait-of-young-woman-in-flowing-gown.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    alt: 'Product 3',
+  },
+  {
+    id: '4',
+    url: 'https://images.pexels.com/photos/31278457/pexels-photo-31278457/free-photo-of-cozy-autumn-portrait-of-woman-in-knit-sweater.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    alt: 'Product 4',
+  },
+];
+export default function ProductImages() {
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="">
+      <div className=" h-[500px] relative">
+        <Image
+          src={images[index]?.url}
+          alt=""
+          fill
+          className=" object-cover rounded-md"
+          sizes="50vw"
+        />
+      </div>
+      <div className=" flex justify-between gap-4 mt-8 ">
+        {images.map((img, index) => (
+          <div
+            key={img.id}
+            className=" w-1/4 h-32 relative gap-4 mt-8"
+            onClick={() => setIndex(index)}
+          >
+            <Image
+              src={img.url}
+              alt=""
+              fill
+              className=" object-cover rounded-md"
+              sizes="30vw"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
