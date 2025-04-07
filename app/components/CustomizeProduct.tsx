5b9f2d4695e647e52dc62949502ddddd5b619015
Bruce <irabruce10@gmail.com>
2025-04-07 13:14:01 +0200

    Customize product page

--- app/components/CustomizeProduct.tsx
+++ app/components/CustomizeProduct.tsx
@@ -46,7 +46,6 @@ export default function CustomizeProduct({
 
   return (
     <div className=" flex flex-col gap-6">
-      {/* <h4 className=" font-medium ">Choose a Color</h4> */}
       {/* <ul className=" flex items-center gap-3">
         <li className=" w-8 h-8 rounded-full ring-1 ring-gray-300 relative cursor-pointer bg-red-500">
           <div
@@ -57,62 +56,59 @@ export default function CustomizeProduct({
 
         <li className=" w-8 h-8 rounded-full ring-1 ring-gray-300 relative cursor-pointer bg-blue-500"></li>
 
-        <li className=" w-8 h-8 rounded-full ring-1 ring-gray-300 relative cursor-not-allowed bg-gray-500">
-          <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "></div>
-        </li>
+
       </ul> */}
       {product.colors.length > 0 && (
-        <>
-          <h4 className="font-medium">Choose a Color</h4>
-          {product.colors.map((col: any) => (
-            <div key={col} onClick={() => handleOptionSelect(col, 'color')}>
-              {col}{' '}
-              {selectedOptions.color === col && (
-                <span className="text-xs font-bold">Selected</span>
-              )}
-            </div>
-          ))}
-        </>
+        <div className=" flex flex-col gap-4">
+          <h4 className=" font-medium ">Choose a Color</h4>
+          <ul className=" flex items-center gap-3">
+            {product.colors.map((col: any) => (
+              <li
+                key={col}
+                onClick={() => handleOptionSelect(col, 'color')}
+                className=" w-8 h-8 rounded-full ring-1 ring-gray-300 relative cursor-pointer "
+                style={{ backgroundColor: col }}
+              >
+                {selectedOptions.color === col && (
+                  <div
+                    className="absolute w-10 h-10 rounded-full ring-2 top-1/2
+          left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+                    style={{ color: selectedOptions.color }}
+                  ></div>
+                )}
+              </li>
+            ))}
+            <li className=" w-8 h-8 rounded-full ring-1 ring-gray-300 relative cursor-not-allowed bg-gray-500">
+              <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "></div>
+            </li>
+          </ul>
+        </div>
       )}
 
       {product.sizes.length > 0 && (
         <>
-          <h4 className="font-medium">Choose a Size</h4>
-          {product.sizes.map((size: any) => (
-            <div key={size} onClick={() => handleOptionSelect(size, 'size')}>
-              {size}
-              {selectedOptions.size === size && (
-                <span className="text-xs font-bold"> Selected</span>
-              )}
-            </div>
-          ))}
+          <h4 className="font-medium">Choose a Size</h4>{' '}
+          <ul className=" flex items-center gap-3">
+            {product.sizes.map((size: any) => (
+              <div key={size} onClick={() => handleOptionSelect(size, 'size')}>
+                <li
+                  className=" ring-1 ring-[#F35C7A] text-[#F35C7A] rounded-md py-1 px-4 text-sm cursor-pointer "
+                  style={{
+                    backgroundColor:
+                      selectedOptions.size === size ? '#F35C7A' : '',
+                    color: selectedOptions.size === size ? 'white' : '',
+                  }}
+                >
+                  {size}
+                </li>
+              </div>
+            ))}
+            <li className=" ring-1 ring-[#F35C7A] text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed ">
+              large
+            </li>
+          </ul>
         </>
       )}
-      {/* {sizes.map((size) => (
-        <li className=" ring-1 ring-[#F35C7A] text-[#F35C7A] rounded-md py-1 px-4 text-sm cursor-pointer " key={size[0]}>
-          {size[0]}
-        </li>
-      ))} */}
-      {/* {colors.map((color) => (
-        <div className=" flex  flex-col gap-4" key={color[0]}>
-          {colors[0]}
-        </div>
-      ))} */}
-      <h4 className=" font-medium ">Choose a Size</h4>
-      <ul className=" flex items-center gap-3">
-        <li className=" ring-1 ring-[#F35C7A] text-[#F35C7A] rounded-md py-1 px-4 text-sm cursor-pointer ">
-          small
-        </li>
-        <li className=" ring-1 ring-[#F35C7A] bg-[#F35C7A] text-white rounded-md py-1 px-4 text-sm cursor-pointer ">
-          medium
-        </li>
-        <li className=" ring-1 ring-[#F35C7A] text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed ">
-          large
-        </li>
-        <li className=" ring-1 ring-[#F35C7A] text-[#F35C7A] rounded-md py-1 px-4 text-sm cursor-pointer ">
-          extra large
-        </li>
-      </ul>
     </div>
   );
 }
