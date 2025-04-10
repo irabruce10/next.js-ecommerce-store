4dd636e4eb7c2cd74006ec0c581ad7d9cf8a237a
Bruce <irabruce10@gmail.com>
2025-04-10 21:32:16 +0200

    Deployment

--- app/redux/CartSlide.ts
+++ app/redux/CartSlide.ts
@@ -31,14 +31,24 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addItem: (state, action: PayloadAction<Omit<CartItem, 'quantity'>>) => {
+    // addItem: (state, action: PayloadAction<Omit<CartItem, 'quantity'>>) => {
+    //   const existingItem = state.items.find(
+    //     (item) => item.id === action.payload.id,
+    //   );
+    //   if (existingItem) {
+    //     existingItem.quantity += 1;
+    //   } else {
+    //     state.items.push({ ...action.payload, quantity: 1 });
+    //   }
+    // },
+    addItem: (state, action: PayloadAction<CartItem>) => {
       const existingItem = state.items.find(
         (item) => item.id === action.payload.id,
       );
       if (existingItem) {
         existingItem.quantity += 1;
       } else {
-        state.items.push({ ...action.payload, quantity: 1 });
+        state.items.push(action.payload);
       }
     },
 
@@ -92,68 +102,3 @@ export const { addItem, removeFromCart, deleteFromCart, clearCart } =
   cartSlice.actions;
 
 export default cartSlice.reducer;
-
-// const addDecimals = (num) => {
-//   return (Math.round(num * 100) / 100).toFixed(2);
-// };
-
-// const cartSlice = createSlice({
-//   name: 'cart',
-//   initialState,
-//   reducers: {
-//     addToCart: (state, action) => {
-//       const existingItem = state.cartItems.find(
-//         (item) => item.id === action.payload.id,
-//       );
-//       if (existingItem?.id === action.payload.id) {
-//         // existingItem.quantity += action.payload.quantity;
-//         state.cartItems = state.cartItems.map((item) =>
-//           item.id === existingItem.id ? action.payload : item,
-//         );
-//       } else {
-//         state.cartItems = [...state.cartItems, action.payload];
-//       }
-
-//       state.itemsPrice = addDecimals(
-//         state.cartItems.reduce(
-//           (acc, item) => acc + item.price * item.quantity,
-//           0,
-//         ),
-//       );
-//       state.totalPrice = addDecimals(Number(state.itemsPrice));
-//       // cookies.set('carts', JSON.stringify(state));
-//     },
-
-//     removeFromCart: (state, action) => {
-//       const index = state.cartItems.findIndex(
-//         (item) => item.id === action.payload,
-//       );
-//       state.cartItems.splice(index, 1);
-
-//       state.itemsPrice = addDecimals(
-//         state.cartItems.reduce(
-//           (acc, item) => acc + item.price * item.quantity,
-//           0,
-//         ),
-//       );
-//       state.totalPrice = addDecimals(Number(state.itemsPrice));
-//     },
-//     // updateQuantity: (state, action) => {
-//     //   const existingItem = state.find((item) => item.id === action.payload.id);
-//     //   if (existingItem && action.payload.quantity > 0) {
-//     //     existingItem.quantity = action.payload.quantity;
-//     //   }
-//     // },
-
-//     hideLoading: (state) => {
-//       state.loading = false;
-//     },
-//   },
-// });
-
-// export const CartSlideReducer = cartSlice.reducer;
-
-// export const { addToCart, removeFromCart, updateQuantity, hideLoading } =
-//   cartSlice.actions;
-
-// export const getCart = (state) => state.cartItems;
