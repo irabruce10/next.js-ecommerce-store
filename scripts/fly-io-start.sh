b0a320375d0244b2c01958a9b51b2e6cc69433e8
Bruce <irabruce10@gmail.com>
2025-04-10 23:21:43 +0200

    Deployment

--- scripts/fly-io-start.sh
+++ scripts/fly-io-start.sh
@@ -1,3 +1,18 @@
+# #!/usr/bin/env bash
+
+# # Exit if any command exits with a non-zero exit code
+# set -o errexit
+
+# if [[ ! -f /postgres-volume/run/postgresql/data/postgresql.conf ]]; then
+#   echo "❗️ No PostgreSQL database found, run the setup script"
+#   sleep infinity
+# fi
+
+# echo "Setting up PostgreSQL on Fly.io..."
+# su postgres -c "pg_ctl start -D /postgres-volume/run/postgresql/data"
+
+# pnpm migrate up
+# ./node_modules/.bin/next start
 #!/usr/bin/env bash
 
 # Exit if any command exits with a non-zero exit code
@@ -9,7 +24,7 @@ if [[ ! -f /postgres-volume/run/postgresql/data/postgresql.conf ]]; then
 fi
 
 echo "Setting up PostgreSQL on Fly.io..."
-su postgres -c "pg_ctl start -D /postgres-volume/run/postgresql/data"
+su postgres -c "pg_ctl start --pgdata=/postgres-volume/run/postgresql/data"
 
 pnpm migrate up
 ./node_modules/.bin/next start
